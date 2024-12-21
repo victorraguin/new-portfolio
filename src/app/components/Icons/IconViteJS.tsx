5d9e5a133a41b17e92cc4f09c0658a69d75f824a
Victor Raguin <victor.raguin@live.fr>
2024-12-21 12:46:14 +0100

    ✨ : update my CV and the project section

---
+++ src/app/components/Icons/IconViteJS.tsx
@@ -0,0 +1,19 @@
+// icon:tailwind | Material Design Icons https://materialdesignicons.com/ | Austin Andrews
+import * as React from 'react'
+
+function IconViteJS (props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      fill='currentColor'
+      height='1.4em'
+      width='1.4em'
+      {...props}
+    >
+      <title>flash</title>
+      <path d='M7,2V13H10V22L17,10H13L17,2H7Z' />
+    </svg>
+  )
+}
+
+export default IconViteJS
